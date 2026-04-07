8f31814f81accbd637e1f216d9c0cb771b71a506
Vaishnavi Thakare <your-github-email@example.com>
2026-04-07 22:03:34 +0530

    Fix Clerk webhook errors

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -1,48 +1,55 @@
-import { webhook } from "svix";
+import { Webhook } from "svix";
 import User from "../models/User.js";
 
-//API controller function to Manage Clerk User database
 export const clerkWebhooks = async (req, res) => {
   try {
-    const whook = new webhook(process.env.CLERK_WEBHOOK_SECRET);
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
     await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
-      "svix-timestam": req.headers["scix-timestamp"],
+      "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     });
+
     const { data, type } = req.body;
 
     switch (type) {
       case "user.created": {
         const userData = {
           _id: data.id,
-          email: data.email_address[0].email_address,
-          name: data.first_name + "" + data.last_name,
-          imageUrl: data.image_url,
-        };
-        await User.create(userDta);
-        res.json({});
-        break;
-      }
-      case "user.updated": {
-        const userData = {
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
-        await User.findByIdAndUpdate(data.id, userData);
-        res.json({});
-        break;
+
+        await User.create(userData);
+        return res.json({ success: true });
       }
+
+      case "user.updated": {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          imageUrl: data.image_url,
+        };
+
+        await User.findByIdAndUpdate(data.id, userData);
+        return res.json({ success: true });
+      }
+
       case "user.deleted": {
         await User.findByIdAndDelete(data.id);
-        res.json({});
-        break;
+        return res.json({ success: true });
       }
+
       default:
-        break;
+        return res.json({ success: false });
     }
   } catch (error) {
-    res.json({ success: false, mesage: error.message });
+    console.error(error);
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
