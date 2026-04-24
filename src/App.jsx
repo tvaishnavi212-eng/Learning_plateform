af3f87df2cd78ef486d7994a2e0b57f275359765
Vaishnavi Thakare <your-github-email@example.com>
2026-04-24 14:03:22 +0530

    Updated features and improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import { Route, Routes, useMatch } from "react-router-dom";
 import Home from "./pages/student/Home";
 import CoursesList from "./pages/student/CoursesList";
